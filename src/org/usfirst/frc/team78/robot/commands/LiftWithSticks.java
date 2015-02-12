607f1e152fd5a25fde1262a9e2d4712a0d0505e3
tfleig78 <tyler.fleig@hotmal.com>
2015-02-11 22:57:19 -0500

    Fixed stick error

--- src/org/usfirst/frc/team78/robot/commands/LiftWithSticks.java
+++ src/org/usfirst/frc/team78/robot/commands/LiftWithSticks.java
@@ -23,7 +23,7 @@ public class LiftWithSticks extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	Robot.elevator.liftWithSticks();
-    	SmartDashboard.putNumber("Stick Val", Robot.oi.getManipulatorLeftStick());
+    	SmartDashboard.putNumber("Stick Val", Robot.oi.getManipulatorLeftStickY());
     }
 
     // Make this return true when this Command no longer needs to run execute()
